4a847fd169cea88db55a453ce157a29a29b8a68b
RD-Vijay <160385614+RD-Vijay@users.noreply.github.com>
2024-02-20 08:33:21 +0530

    Commit
    Added

--- AutomateTestingPuzzle/obj/Debug/net8.0/AutomateTestingPuzzle.AssemblyInfo.cs
+++ AutomateTestingPuzzle/obj/Debug/net8.0/AutomateTestingPuzzle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomateTestingPuzzle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3afe74f0f87ab111d6a953445658e7124e823099")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomateTestingPuzzle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomateTestingPuzzle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
